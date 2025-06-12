64e58c17b29250ce7380a3786c79d7c84b7c743d
NarriGoud <160447366+NarriGoud@users.noreply.github.com>
2025-06-12 21:05:59 +0530

    Update products_entry.py

--- products_entry.py
+++ products_entry.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import gspread
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Google Sheets setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("ascendant-timer-352404-a0a09f9e6e2a.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    json.loads(st.secrets["GSPREAD_KEY"]), scope
+)
 client = gspread.authorize(creds)
 
 sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1Fx-4Y9O4mmcI2PbpEehipAacc3OXhgGQDUcO3KOkkI8/edit").sheet1
